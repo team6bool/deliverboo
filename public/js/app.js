3156b984e5dce9d83337a051a8b69351048c756a
Al-E-00 <mazzellaalessio6@gmail.com>
2022-09-15 12:14:00 +0200

    toggle button works

--- public/js/app.js
+++ public/js/app.js
@@ -40232,8 +40232,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Fonzie\documents\boolean\fonzie code\deliverboo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Fonzie\documents\boolean\fonzie code\deliverboo\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/alessiomazzella/Documents/BOOLEAN/final_project/deliverboo/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/alessiomazzella/Documents/BOOLEAN/final_project/deliverboo/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
