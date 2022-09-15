188554c2cd92ff687e673499540d831e275ddf6b
Spud97 <luca.dercole97@gmail.com>
2022-09-15 17:30:08 +0200

    creato seeder categorie mancano le immagini

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -11,6 +11,76 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $myCategories = [
+            [
+                "Categoria" => "Sushi",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Kebab",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Pizza",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Poke",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Burger",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Veg",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Carne",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Pesce",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Glutenfree",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Pasta",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Thai",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Indiano",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Gourmet",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Torte",
+                "Immagine" => "",
+            ],
+            [
+                "Categoria" => "Gelato",
+                "Immagine" => "",
+            ],
+        ];
+
+        foreach ($myCategories as $category) {
+            $data = new Category();
+
+            $data->Categoria = $category["Categoria"];
+            $data->Immagine = $category["Immagine"];
+
+            $data->save();
+        }
     }
 }
