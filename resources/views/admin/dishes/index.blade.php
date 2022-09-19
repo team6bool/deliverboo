9fe8196224a0d0e5f311aa45c60e249f69a0fc13
Al-E-00 <mazzellaalessio6@gmail.com>
2022-09-19 22:46:27 +0200

    d-flex, cols-4 implemented index dishes

--- resources/views/admin/dishes/index.blade.php
+++ resources/views/admin/dishes/index.blade.php
@@ -1,16 +1,22 @@
 @extends('layouts.app')
 
 @section('content')
-    @foreach ($dishes as $dish)
-        <div class="card" style="width: 18rem;">
-            <img src="{{ asset('/storage/public/images/dishes/' . $dish->img) }}" alt="{{ $dish->name }}">
-            <div class="card-body">
-                <h5 class="card-title">{{ $dish->name }}</h5>
-                <p class="card-text">{{ $dish->description }}</p>
-                <p class="card-text">{{ $dish->user->name }}</p>
-                <p class="card-text">{{ $dish->price }}</p>
-                <a href="#" class="btn btn-primary">Edit</a>
-            </div>
+    <div class="container">
+        <div class="row cols-4">
+            @foreach ($dishes as $dish)
+                <div class="col d-flex">
+                    <div class="card" style="width: 18rem;">
+                        <img src="{{ asset('/storage/public/images/dishes/' . $dish->img) }}" alt="{{ $dish->name }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $dish->name }}</h5>
+                            <p class="card-text">{{ $dish->description }}</p>
+                            <p class="card-text">{{ $dish->user->name }}</p>
+                            <p class="card-text">{{ $dish->price }}</p>
+                            <a href="#" class="btn btn-primary">Edit</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
         </div>
-    @endforeach
+    </div>
 @endsection
