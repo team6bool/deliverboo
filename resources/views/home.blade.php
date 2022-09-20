abf64a2ef3c6b7a5e31cf10b264f1b013205297d
Al-E-00 <mazzellaalessio6@gmail.com>
2022-09-20 09:56:40 +0200

    button dashboard - dishes window - orders window

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
 
-                    {{ __('You are logged in!') }}
+                        {{ __('You are logged in!') }}
+                    </div>
+                    <div class="d-flex gap-4">
+                        <button class="btn btn-warning" type="button"
+                            onclick="window.location=`{{ url('admin/dishes') }}`">Piatti</button>
+                        <button class="btn btn-warning" type="button"
+                            onclick="window.location=`{{ url('admin/orders') }}`">Ordini</button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
