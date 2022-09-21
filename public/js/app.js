cc9acf573e51876afd2a6e0187b8b4e6fc616358
Spud97 <luca.dercole97@gmail.com>
2022-09-21 13:14:12 +0200

    sistemato bug con npm run

--- public/js/app.js
+++ public/js/app.js
@@ -40232,8 +40232,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Users/alessiomazzella/Documents/BOOLEAN/final_project/deliverboo/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/alessiomazzella/Documents/BOOLEAN/final_project/deliverboo/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\lucad\Boolean\deliverboo-1\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\lucad\Boolean\deliverboo-1\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
