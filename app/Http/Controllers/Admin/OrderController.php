c1ee4d5530bd0fb6a8a0159d76e8b2e5ed23dbdd
SeanBitter <paolodig@hotmail.it>
2022-09-22 13:19:04 +0200

    Adds orders control

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -18,7 +18,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        $orders = Order::orderBy("created_at", "desc")->get();
+        $orders = auth()->user()->orders()->orderBy('created_at', 'desc')->get();
 
         return view("admin.orders.index", compact("orders"));
     }
