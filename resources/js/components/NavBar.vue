1d52c8c9d678aeb6f6f70c7c8f8ed09f5df4bef3
Spud97 <luca.dercole97@gmail.com>
2022-09-23 18:17:38 +0200

    tasto accedi

--- resources/js/components/NavBar.vue
+++ resources/js/components/NavBar.vue
@@ -8,7 +8,7 @@
                 />
             </a>
             <div class="nav-buttons">
-                <button class="btn-secondary"><a href="/register">Accedi</a></button>
+                <button class="btn-secondary"><a href="/login">Accedi</a></button>
                 <router-link :to="{ name: 'cart.index' }"
                     ><button class="btn-cart">
                         <i class="fa fa-solid fa-cart-shopping"></i></button
